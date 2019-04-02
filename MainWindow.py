e57314bca7fb04d796636b39b96b591f3c7b96fa
Jean Lapostolle <lapostolle.jean@gmail.com>
2019-04-02 15:44:41 +0200

    MainWindow design

--- MainWindow.py
+++ MainWindow.py
@@ -1,1 +1,20 @@
-from PyQt5 import *
+import sys
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5 import uic
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent = None):
+        QMainWindow.__init__(self, parent )
+        self.resize(640, 820)
+        uic.loadUi('ui/mainwindow.ui', self)
+
+
+
+if __name__=="__main__":
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+    app.exec_()
